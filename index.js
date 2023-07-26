d277b3377b93f51f1d326c87506170c8eb1b4483
Mate Telkes <95150943+MatthewTelkes@users.noreply.github.com>
2023-07-26 16:50:45 +0200

    Delete hover, Add Onclick function

---
+++ index.js
@@ -0,0 +1,58 @@
+function changeImage(fileName) {
+  let img = document.querySelector("#cardOne");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageTwo(fileName) {
+  let img = document.querySelector("#cardTwo");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageThree(fileName) {
+  let img = document.querySelector("#cardThree");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageFour(fileName) {
+  let img = document.querySelector("#cardFour");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageFive(fileName) {
+  let img = document.querySelector("#cardFive");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageSix(fileName) {
+  let img = document.querySelector("#cardSix");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageSeven(fileName) {
+  let img = document.querySelector("#cardSeven");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageEight(fileName) {
+  let img = document.querySelector("#cardEight");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageNine(fileName) {
+  let img = document.querySelector("#cardNine");
+  img.setAttribute("src", fileName);
+}
+function changeImageTen(fileName) {
+  let img = document.querySelector("#cardTen");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageEleven(fileName) {
+  let img = document.querySelector("#cardEleven");
+  img.setAttribute("src", fileName);
+}
+
+function changeImageTwelve(fileName) {
+  let img = document.querySelector("#cardTwelve");
+  img.setAttribute("src", fileName);
+}
